0719329be92e7b4b14f7edeabe19d350a4326f5a
zhouxiaofei <mrzhouxiaofei@foxmail.com>
2017-12-21 22:45:29 +0800

    登录

--- src/com/mt/dao/UserDao.java
+++ src/com/mt/dao/UserDao.java
@@ -1,5 +1,22 @@
 package com.mt.dao;
 
+import com.mt.model.User;
+import com.mt.utils.JDBCUtils;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+
+import java.sql.SQLException;
+
 public class UserDao {
 
+    public User login(User user) throws SQLException {
+        QueryRunner queryRunner = new QueryRunner(JDBCUtils.getDataSource());
+        String sql = "select * from users where phone = ? and password = ?";
+        Object[] params = {
+                user.getPhone(),
+                user.getPassword()
+        };
+        User u = queryRunner.query(sql, new BeanHandler<User>(User.class), params);
+        return u;
+    }
 }
--- src/com/mt/servlet/login/LoginServlet.java
+++ src/com/mt/servlet/login/LoginServlet.java
@@ -1,19 +1,46 @@
 package com.mt.servlet.login;
 
+import com.mt.model.User;
+import com.mt.service.UserService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @WebServlet(name = "LoginServlet")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        // 获取数据
+        String phone = request.getParameter("phone");
+        String password = request.getParameter("password");
+        // 封装数据
+        User user = new User();
+        user.setPhone(phone);
+        user.setPassword(password);
+        // 处理数据
+        UserService userService = new UserService();
+        User u;
+        try {
+            u = userService.login(user);
+            // 页面跳转
+            if (u != null) {
+                // 查询到,可以登录,同时将user信息存进session
+                request.getSession().setAttribute("user", u);
+                request.getRequestDispatcher("/IndexServlet").forward(request, response);
+            } else {
+                request.setAttribute("msg", "你输入的用户名或密码有误");
+                request.getRequestDispatcher("/home/login/login.jsp").forward(request, response);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
